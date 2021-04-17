7988559e8d69d08f67b5bd8764ec3b5a620761fd
Daniel Osborne <daniel.osborne@pm.me>
2021-04-17 19:52:32 +0100

    Implemented channels, to better sync the GoRoutines

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func main() {
 	// Create a Mutex, once against using a pointer
 	m := &sync.RWMutex{}
 
+	cacheCh := make(chan Book)
+	dbCh := make(chan Book)
+
 	for i := 0; i < 10; i++ {
 		// Fetch a random Book ID
 		id := rnd.Intn(10) + 1
@@ -25,27 +28,41 @@ func main() {
 		// We've got 2 GoRoutines to wait for
 		wg.Add(2)
 
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			// Query the cache, if found then print it out
 			if b, ok := queryCache(id, m); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
+				ch <- b // Pass the found Book to the cache channel
 			}
 
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, cacheCh)
 
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- Book) {
 			// Query the DB, if found then print it out
 			if b, ok := queryDatabase(id, m); ok {
-				fmt.Println("from database")
-				fmt.Println(b)
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
 			}
 
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, dbCh)
 
-		//time.Sleep(150 * time.Millisecond)
+		// Create on GoRoutine per query to handle response
+		go func(cacheCh, dbCh <-chan Book) {
+			select {
+			case b := <-cacheCh:
+				fmt.Println("Source: Cache")
+				fmt.Println(b)
+				<-dbCh // Wait to get the message from the DB Channel so we don't block
+			case b := <-dbCh:
+				fmt.Println("Source: Database")
+				fmt.Println(b)
+			}
+		}(cacheCh, dbCh)
+
+		time.Sleep(150 * time.Millisecond)
 	}
 
 	wg.Wait()

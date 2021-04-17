a0f0fd4ebd03623574697d0dcc79e862a72b093b
Daniel Osborne <daniel.osborne@pm.me>
2021-04-17 17:38:41 +0100

    Implemented Wait Groups, and Mutexes/RWMutexes to prevent race conditions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -11,45 +12,65 @@ var cache = map[int]Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
+	// Create a Wait Group, and save the pointer
+	wg := &sync.WaitGroup{}
+
+	// Create a Mutex, once against using a pointer
+	m := &sync.RWMutex{}
+
 	for i := 0; i < 10; i++ {
 		// Fetch a random Book ID
 		id := rnd.Intn(10) + 1
 
-		// Query the cache, if found then print it out
-		if b, ok := queryCache(id); ok {
-			fmt.Println("from cache")
-			fmt.Println(b)
-			continue
-		}
+		// We've got 2 GoRoutines to wait for
+		wg.Add(2)
 
-		// Query the DB, if found then print it out
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println("from database")
-			fmt.Println(b)
-			continue
-		}
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			// Query the cache, if found then print it out
+			if b, ok := queryCache(id, m); ok {
+				fmt.Println("from cache")
+				fmt.Println(b)
+			}
 
-		// Could not find a book with the given ID
-		fmt.Printf("Book not found with ID: '%v'", id)
-		time.Sleep(150 * time.Millisecond)
+			wg.Done()
+		}(id, wg, m)
+
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			// Query the DB, if found then print it out
+			if b, ok := queryDatabase(id, m); ok {
+				fmt.Println("from database")
+				fmt.Println(b)
+			}
+
+			wg.Done()
+		}(id, wg, m)
+
+		//time.Sleep(150 * time.Millisecond)
 	}
+
+	wg.Wait()
 }
 
 // Accepts the ID of the Book, and returns a true/false to whether it was found
-func queryCache(id int) (Book, bool) {
+func queryCache(id int, m *sync.RWMutex) (Book, bool) {
+	// Lock for Writes, however, allowing multiple reads is okay
+	m.RLock()
 	b, ok := cache[id]
+	m.RUnlock()
 	return b, ok
 }
 
 // Simulates running the query against the database - this method is slower than
 // querying the cache
-func queryDatabase(id int) (Book, bool) {
+func queryDatabase(id int, m *sync.RWMutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 
 	for _, b := range books {
 		if b.ID == id {
 			// Add the Book to the cache
+			m.Lock()
 			cache[id] = b
+			m.Unlock()
 
 			// Return the Book
 			return b, true
